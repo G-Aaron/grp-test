ed0696083eb8bf5239ad7ac793b66717740e9aec
高仁鹏 <gaorenpeng@zytcft.com>
2019-01-16 16:49:40 +0800

    线程池 连接池 试验

---
+++ grp-thread-test/src/main/java/com/grp/test/thread/app/ConnectionPool.java
@@ -0,0 +1,14 @@
+package com.grp.test.thread.app;
+
+import java.util.concurrent.CountDownLatch;
+
+/**
+ * <p>Title: 连接池 简单实现</p>
+ * <p>Description: </p>
+ *
+ * @version 1.0
+ * @author: gaorenpeng
+ * @date: 2019-01-16 16:43
+ **/
+public class ConnectionPool {
+}
---
+++ grp-thread-test/src/main/java/com/grp/test/thread/app/ThreadPool.java
@@ -0,0 +1,12 @@
+package com.grp.test.thread.app;
+
+/**
+ * <p>Title: 线程池 简单实现</p>
+ * <p>Description: </p>
+ *
+ * @version 1.0
+ * @author: gaorenpeng
+ * @date: 2019-01-16 16:43
+ **/
+public class ThreadPool {
+}
